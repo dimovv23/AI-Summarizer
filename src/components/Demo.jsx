de2798e90bb34896046d909b8c14715d1c18fb6f
dimovv23 <ggdimovv@gmail.com>
2024-02-20 18:34:25 +0200

    Added some of the logic in the Demo component

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,39 @@
 import { useState, useEffect } from "react";
+import { IoMdLink } from "react-icons/io";
 
 const Demo = () => {
-  return <section className="demo-section"></section>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleChange = (e) => {
+    setArticle({
+      ...article,
+      url: e.target.value,
+    });
+  };
+
+  return (
+    <section className="demo-section">
+      <div className="search-container">
+        <form onSubmit={() => {}}>
+          <IoMdLink className="link-icon" />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit">⏎</button>
+        </form>
+
+        {/*TODO: URL history */}
+      </div>
+      {/* TODO: Display results */}
+    </section>
+  );
 };
 
 export default Demo;
